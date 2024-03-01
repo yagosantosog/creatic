3c3671e709a05d30c2ba2af88e9b356f45c97cbc
yagosantosog <santosyago2005@gmail.com>
2024-03-01 13:05:02 -0300

    add style to header

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './cta.css'
 
-const Cta = () => {
+
+
+const Cta = ({theme, title, text, btn_text }) => {
   return (
-    <div>
-      
+    <div className='creatic__cta'>
+      <p>{theme}</p>
+      <h2>{title}</h2>
+      <div className="color-bar"></div>
+      <p>{text}</p>
+      <div className="creatic__cta-btn">
+        <button type='button'>{btn_text}</button>
+      </div>
     </div>
   )
 }
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import './header.css';
+import { Cta } from '../../components';
+import jewelWoman from '../../assets/beautiful-woman-with-big-jewel.png'
 
 const Header = () => {
   return (
-    <div>
-      
+    <div className='creatic__header'>
+      <div className='original_gradient'></div>
+      <div className="creatic__header-container">
+        <div className="creatic__header-container_content">
+        <Cta theme='WELCOME TO CREATIC' title={<span>WE ARE <span className='span'>CREATIVE</span> DESIGN AGENCY</span>} text='Lorem ipsumNeque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit Neque porro elit Neque porro quis ipsum' btn_text='GET IN TOUCH'/>
+        </div>
+        <div className="creatic__header-container_image">
+          <img src={jewelWoman} alt="A beautiful woman with a big jewel" />
+        </div>
+        </div>
     </div>
   )
 }
